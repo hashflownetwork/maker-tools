4e5d553b8209ec1a0736e273fae1cd39af2a6788
Sameer Brenn <sameer@hashflow.com>
2023-12-11 15:43:40 -0700

    Check round-trip levels

--- src/helpers/levels.test.ts
+++ src/helpers/levels.test.ts
@@ -14,49 +14,57 @@ function computeLevelsWrapper(
 }
 
 describe('computeLevelsQuote', () => {
-  test('computes a proper selllevels quote given a quotetokenamount', () => {
+  test('computes a proper sell levels quote given a quotetokenamount', () => {
     expect(
       computeLevelsWrapper(
         [
-          {
-            q: '0',
-            p: '0.99009900990099009901',
-          },
-          {
-            q: '50.5',
-            p: '0.99009900990099009901',
-          },
-          {
-            q: '110',
-            p: '0.90909090909090909091',
-          },
+          { q: '0', p: '0.99009900990099009901' },
+          { q: '50.5', p: '0.99009900990099009901' },
+          { q: '110', p: '0.90909090909090909091' },
         ],
         undefined,
         BigNumber('40')
       )
     ).toBeCloseTo(40.4);
   });
-  test('computes a proper selllevels quote given a basetokenamount', () => {
+  test('computes a proper sell levels quote given a basetokenamount', () => {
     expect(
       Number(
         computeLevelsQuote(
           [
-            {
-              q: '0',
-              p: '0.99009900990099009901',
-            },
-            {
-              q: '50.5',
-              p: '0.99009900990099009901',
-            },
-            {
-              q: '110',
-              p: '0.90909090909090909091',
-            },
+            { q: '0', p: '0.99009900990099009901' },
+            { q: '50.5', p: '0.99009900990099009901' },
+            { q: '110', p: '0.90909090909090909091' },
           ],
           BigNumber('40.4'),
           undefined
         ).amount?.toString()
+      )
+    ).toBeCloseTo(40);
+  });
+  test('computes a proper buy levels quote given a quotetokenamount', () => {
+    expect(
+      computeLevelsWrapper(
+        [
+          { q: '0', p: '0.99' },
+          { q: '50.0', p: '0.99' },
+          { q: '100.0', p: '0.9' },
+        ],
+        undefined,
+        BigNumber('40')
+      )
+    ).toBeCloseTo(40.4);
+  });
+  test('computes a proper buy levels quote given a quotetokenamount', () => {
+    expect(
+      computeLevelsWrapper(
+        [
+          { q: '0', p: '0.99' },
+          { q: '50.0', p: '0.99' },
+          { q: '100.0', p: '0.9' },
+        ],
+        undefined,
+        BigNumber('40.4')
       )
     ).toBeCloseTo(40);
   });
