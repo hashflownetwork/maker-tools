3e5627a08d47761e5dc520e43f24c560d2123727
Sameer Brenn <sameer@hashflow.com>
2023-12-11 14:52:53 -0700

    Add test

--- src/helpers/levels.test.ts
+++ src/helpers/levels.test.ts
@@ -1,11 +1,22 @@
 import BigNumber from 'bignumber.js';
+import { PriceLevel } from 'helpers/types';
 
 import { computeLevelsQuote } from './levels';
+
+function computeLevelsWrapper(
+  priceLevels: PriceLevel[],
+  reqBaseAmount?: BigNumber,
+  reqQuoteAmount?: BigNumber
+) {
+  return Number(
+    computeLevelsQuote(priceLevels, reqBaseAmount, reqQuoteAmount).amount
+  );
+}
 
 describe('computeLevelsQuote', () => {
   test('computes a proper selllevels quote given a quotetokenamount', () => {
     expect(
-      computeLevelsQuote(
+      computeLevelsWrapper(
         [
           {
             q: '0',
@@ -22,29 +33,31 @@ describe('computeLevelsQuote', () => {
         ],
         undefined,
         BigNumber('40')
-      ).amount?.toString()
-    ).toBe('40.4');
+      )
+    ).toBeCloseTo(40.4);
   });
   test('computes a proper selllevels quote given a basetokenamount', () => {
     expect(
-      computeLevelsQuote(
-        [
-          {
-            q: '0',
-            p: '0.99009900990099009901',
-          },
-          {
-            q: '50.5',
-            p: '0.99009900990099009901',
-          },
-          {
-            q: '110',
-            p: '0.90909090909090909091',
-          },
-        ],
-        undefined,
-        BigNumber('40.4')
-      ).amount?.toString()
-    ).toBe('40');
+      Number(
+        computeLevelsQuote(
+          [
+            {
+              q: '0',
+              p: '0.99009900990099009901',
+            },
+            {
+              q: '50.5',
+              p: '0.99009900990099009901',
+            },
+            {
+              q: '110',
+              p: '0.90909090909090909091',
+            },
+          ],
+          BigNumber('40.4'),
+          undefined
+        ).amount?.toString()
+      )
+    ).toBeCloseTo(40);
   });
 });
