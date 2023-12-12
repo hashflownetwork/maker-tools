760308e181cf9e03750d0132868444ecde3c3ba2
geoffrey-hashflow <135063042+geoffrey-hashflow@users.noreply.github.com>
2023-12-11 16:54:56 -0800

    Update levels.test.ts

--- src/helpers/levels.test.ts
+++ src/helpers/levels.test.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import { PriceLevel } from 'helpers/types';
+import { PriceLevel } from '../helpers/types';
 
 import { computeLevelsQuote } from './levels';
 
